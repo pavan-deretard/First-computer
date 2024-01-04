01fa051b6c59e5073375732fec7ed92b6d027a46
pavan-deretard <123799786+pavan-deretard@users.noreply.github.com>
2024-01-04 11:21:42 +0530

    Update program_counter.v

--- program_counter.v
+++ program_counter.v
@@ -1,55 +1,56 @@
 `timescale 1ns/1ns
-module program_counter(input wire pc_inc,rst,clk,jmp, input	wire[4:0] jmp_add,	output reg[4:0] add);
-always@(clk) begin
-	if(clk) begin
-	if(!rst) begin
-		if(jmp) begin
-		add <=jmp_add;
-		end
-		else if(pc_inc) begin
-		add=add+4;
-		end
-	end
+module program_counter(pc_inc,rst,clk,jmp,jmp_add,add);
+input wire pc_inc,rst,clk,jmp;
+input wire[4:0] jmp_add;
+output reg[4:0] add;
+always @(posedge clk,posedge jmp,posedge rst) begin
+    if (rst) begin
+      add <= 0;
+    end 
+	 else if(clk) begin
+      if (jmp) begin
+        add <= jmp_add;
+      end else if (pc_inc) begin
+        add <= add + 1;
+      end
+    end
 
-else begin
-add=0;
-end
-end
-end
+  end
 
 endmodule
 
-module pc_tb;
-wire[4:0] add;
+module pc_tb();
 reg pc_inc,rst,clk,jmp;
 reg[4:0] jmp_add;
+wire[4:0] add;
 
-program_counter pc1(pc_inc,rst,clk,jmp,jmp_add,add);
+program_counter pc(pc_inc,rst,clk,jmp,jmp_add,add);
+
+always #50 clk=~clk;
+
 initial begin
-clk=1;
-	forever begin
-		
-		#50
-		clk=~clk;
-		end
-end
-initial begin
-	
-	rst=1;
-	jmp=0;
-	pc_inc=0;
-	jmp_add=0;
-	
-	#100
-	rst=0;
-	pc_inc=1;
-	#500
-	jmp=1;
-	jmp_add=34;
-	#50;
-	jmp=0;
-	rst=1;
-	#5000
-	$finish();
+clk=0;
+rst=1;
+pc_inc=$random;
+jmp=$random;
+jmp_add=$random;
+#100;
+rst=0;
+pc_inc=1;
+jmp=0;
+jmp_add=$random;
+#300
+pc_inc=1;
+jmp=$random;
+jmp_add=$random;
+#300;
+pc_inc=0;
+jmp=$random;
+jmp_add=$random;
+#300
+#23
+rst=1;
+#40
+$finish();
 end
 endmodule
